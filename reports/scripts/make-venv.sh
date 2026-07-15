9ced667d6bd9e4e9140298615010a0af7a1f7a93
Dave Walker <david.walker5@icloud.com>
2026-07-15 13:17:28 +0100

    Added the team summary report to the application

---
+++ reports/scripts/make-venv.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+export REPORTS_ROOT=$( cd "$(dirname "$0")/.." ; pwd -P )
+cd "$REPORTS_ROOT"
+
+# Deactivate and remove the old virtual environment, if present
+echo "Removing existing Virtual Environment, if present ..."
+deactivate 2> /dev/null || true
+rm -fr venv
+
+# Create a new environment and activate it
+echo "Creating new Virtual Environment ..."
+python -m venv venv
+. venv/bin/activate
+
+# Make sure pip is up to date
+pip install --upgrade pip
+
+# Install the requirements
+pip install -e .
